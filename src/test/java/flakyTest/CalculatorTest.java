c9ae30012bef2783eecb3ae8b46e27b66e72bca9
appflowpsl <113003572+appflowpsl@users.noreply.github.com>
2022-09-13 17:50:29 +0530

    Update CalculatorTest.java

--- src/test/java/flakyTest/CalculatorTest.java
+++ src/test/java/flakyTest/CalculatorTest.java
@@ -12,4 +12,64 @@ public class CalculatorTest {
         int num = (int) (Math.random()*(5-1+1)+1);
         assertEquals(num,2);
     }
+    
+    @Test
+    public void test1() {
+        int num = (int) (Math.random()*(5-1+1)+1);
+        assertEquals(num,2);
+    }
+    
+    @Test
+    public void test2() {
+        int num = (int) (Math.random()*(5-1+1)+1);
+        assertEquals(num,2);
+    }
+    
+    @Test
+    public void test3() {
+        int num = (int) (Math.random()*(5-1+1)+1);
+        assertEquals(num,2);
+    }
+    
+    @Test
+    public void test4() {
+        int num = (int) (Math.random()*(5-1+1)+1);
+        assertEquals(num,2);
+    }
+    
+    @Test
+    public void test5() {
+        int num = (int) (Math.random()*(5-1+1)+1);
+        assertEquals(num,2);
+    }
+    
+    @Test
+    public void test6() {
+        int num = (int) (Math.random()*(5-1+1)+1);
+        assertEquals(num,2);
+    }
+    
+    @Test
+    public void test7() {
+        int num = (int) (Math.random()*(5-1+1)+1);
+        assertEquals(num,2);
+    }
+    
+    @Test
+    public void test8() {
+        int num = (int) (Math.random()*(5-1+1)+1);
+        assertEquals(num,2);
+    }
+    
+    @Test
+    public void test9() {
+        int num = (int) (Math.random()*(5-1+1)+1);
+        assertEquals(num,2);
+    }
+    
+    @Test
+    public void test10() {
+        int num = (int) (Math.random()*(5-1+1)+1);
+        assertEquals(num,2);
+    }
 }
